6b6697706fab63557127de88ba8378a159f134cd
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-08-05 11:17:48 +0530

    day 2 todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import Todo from "./components/Todo";
 
 function App() {
   // This is a simple counter app layout
 
   return (
     <>
-      <Counter />
+      {/* <Counter /> */}
+      <Todo/>
     </>
   );
 }
